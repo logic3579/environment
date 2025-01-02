cfedba3a7a6b42c31b744a31832484c079cc65a7
yakir <yakir3@duck.com>
2025-01-02 22:21:21 +0800

    Update nvim config

--- dotfiles/nvim/lua/config/mylspconfig.lua
+++ dotfiles/nvim/lua/config/mylspconfig.lua
@@ -1,29 +1,22 @@
-local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 vim.lsp.set_log_level("error")
 
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    --vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, vim.tbl_extend('force', bufopts, { desc = "Code Action" }))
-    vim.keymap.set('n', '<leader>cd', vim.lsp.buf.declaration, vim.tbl_extend('force', bufopts, { desc = "Go to Declaration" }))
-    vim.keymap.set('n', '<leader>cD', vim.lsp.buf.definition, vim.tbl_extend('force', bufopts, { desc = "Go to Definition" }))
-    vim.keymap.set('n', '<leader>cK', vim.lsp.buf.hover, vim.tbl_extend('force', bufopts, { desc = "Show Hover Information" }))
-    vim.keymap.set('n', '<leader>ci', vim.lsp.buf.implementation, vim.tbl_extend('force', bufopts, { desc = "Go to Implementation" }))
-    vim.keymap.set('n', '<leader>crf', vim.lsp.buf.references, vim.tbl_extend('force', bufopts, { desc = "Show References" }))
-    vim.keymap.set('n', '<leader>crn', vim.lsp.buf.rename, vim.tbl_extend('force', bufopts, { desc = "Rename Symbol" }))
-    --vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    --vim.keymap.set('n', '<leader>cwa', vim.lsp.buf.add_workspace_folder, bufopts)
-    --vim.keymap.set('n', '<leader>cwr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    --vim.keymap.set('n', '<leader>cwl', function()
-    --    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    --end, bufopts)
-    --vim.keymap.set('n', '<leader>cD', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", bufopts, { desc = "code action" }))
+    vim.keymap.set("n", "<leader>cd", vim.lsp.buf.declaration, vim.tbl_extend("force", bufopts, { desc = "goto declaration" }))
+    vim.keymap.set("n", "<leader>cD", vim.lsp.buf.definition, vim.tbl_extend("force", bufopts, { desc = "goto definition" }))
+    vim.keymap.set("n", "<leader>cK", vim.lsp.buf.hover, vim.tbl_extend("force", bufopts, { desc = "hover documentation" }))
+    vim.keymap.set("n", "<leader>ci", vim.lsp.buf.implementation, vim.tbl_extend("force", bufopts, { desc = "goto inplementation" }))
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, vim.tbl_extend("force", bufopts, { desc = "rename" }))
+    vim.keymap.set("n", "<leader>cR", vim.lsp.buf.references, vim.tbl_extend("force", bufopts, { desc = "goto references" }))
+    vim.keymap.set("n", "<leader>cs", vim.lsp.buf.document_symbol, vim.tbl_extend("force", bufopts, { desc = "document symbol" }))
     vim.keymap.set("n", "<leader>cf", function()
         vim.lsp.buf.format({ async = true })
-    end, vim.tbl_extend('force', bufopts, { desc = "Format Document" }))
+    end, vim.tbl_extend("force", bufopts, { desc = "format document" }))
 end
 
 -- Lua
--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -45,6 +45,7 @@ return {
             require("nvim-treesitter.configs").setup({
                 ensure_installed = {
                     "bash",
+                    "html",
                     "go",
                     "gomod",
                     "gowork",
@@ -74,6 +75,16 @@ return {
             })
         end,
     },
+    --[[ Usage
+    Old text                    Command         New text
+    surr*ound_words             ysiw)           (surround_words)
+    *make strings               ys$"            "make strings"
+    [delete ar*ound me!]        ds]             delete around me!
+    remove <b>HTML t*ags</b>    dst             remove HTML tags
+    'change quot*es'            cs'"            "change quotes"
+    <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+    delete(functi*on calls)     dsf             function calls
+    ]]
     {
         "kylechui/nvim-surround",
         version = "*",
