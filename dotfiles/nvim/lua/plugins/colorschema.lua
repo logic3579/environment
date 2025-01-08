3668f9113bfcd3faa6cb20b853a69063fa96df3d
logic <logic3@duck.com>
2025-01-08 15:22:00 +0800

    Add nvim plugin solarized.nvim

--- dotfiles/nvim/lua/plugins/colorschema.lua
+++ dotfiles/nvim/lua/plugins/colorschema.lua
@@ -1,17 +1,48 @@
 return {
+    -- {
+    --     "svrana/neosolarized.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require("neosolarized").setup({
+    --             comment_italics = true,
+    --             background_set = false,
+    --         })
+    --         vim.cmd.colorscheme("neosolarized")
+    --     end,
+    --     dependencies = {
+    --         "tjdevries/colorbuddy.nvim",
+    --     },
+    -- },
     {
-        "svrana/neosolarized.nvim",
+        "maxmx03/solarized.nvim",
         lazy = false,
         priority = 1000,
-        config = function()
-            require("neosolarized").setup({
-                comment_italics = true,
-                background_set = true,
-            })
-            vim.cmd.colorscheme("neosolarized")
-        end,
-        dependencies = {
-            "tjdevries/colorbuddy.nvim",
+        ---@type solarized.config
+        opts = {
+            transparent = {
+                enabled = true,
+                pmenu = true,
+                normal = true,
+                normalfloat = true,
+                neotree = true,
+                nvimtree = true,
+                whichkey = true,
+                telescope = true,
+                lazy = true,
+            },
+            variant = "autumn",
+            styles = {
+                comments = { italic = true, bold = false },
+                -- functions = { italic = true },
+                -- variables = { italic = false },
+            },
         },
+        config = function(_, opts)
+            -- vim.o.termguicolors = true
+            -- vim.o.background = "light"
+            require("solarized").setup(opts)
+            vim.cmd.colorscheme "solarized"
+        end,
     },
 }
